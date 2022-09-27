99a32d687412304778056de675490f9b6a3a3aa6
RostokaVitaliyRIS211b <rostoka.vit@yandex.ru>
2022-09-27 18:19:18 +0500

    Добавьте файлы проекта.

---
+++ labaDiscretka/GlobalUsings.cs
@@ -0,0 +1,4 @@
+﻿global using SFML.Audio;
+global using SFML.System;
+global using SFML.Window;
+global using SFML.Graphics;
---
+++ labaDiscretka/Main.cs
@@ -0,0 +1,7 @@
+﻿RenderWindow MainWindow = new(new VideoMode(1280, 720), "Lab1");
+while(MainWindow.IsOpen)
+{
+    MainWindow.Clear(Color.White);
+    MainWindow.DispatchEvents();
+    MainWindow.Display();
+}
